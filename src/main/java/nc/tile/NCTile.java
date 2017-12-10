5b9a374622fb2e2de81a848f572f36c8bd437d69
Thutmose <Thutmose@users.noreply.github.com>
2017-12-10 15:12:15 -0500

    Fixes rotation deleting inventory contents.
    Note that shouldRefresh should then also be overriden in each class for the tiles that change block when they activate.

--- src/main/java/nc/tile/NCTile.java
+++ src/main/java/nc/tile/NCTile.java
@@ -1,12 +1,15 @@
 package nc.tile;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 
 public abstract class NCTile extends TileEntity implements ITickable {
 	
@@ -84,5 +87,19 @@ public abstract class NCTile extends TileEntity implements ITickable {
 
 	public void handleUpdateTag(NBTTagCompound tag) {
 		super.handleUpdateTag(tag);
+	}
+	
+	/**
+	 * Called from Chunk.setBlockIDWithMetadata and Chunk.fillChunk, determines if this tile entity should be re-created when the ID, or Metadata changes.
+	 * Use with caution as this will leave straggler TileEntities, or create conflicts with other TileEntities if not used properly.
+	 *
+	 * @param world Current world
+	 * @param pos Tile's world position
+	 * @param oldState The old ID of the block
+	 * @param newState The new ID of the block (May be the same)
+	 * @return true forcing the invalidation of the existing TE, false not to invalidate the existing TE
+	 */
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+		return (oldState.getBlock() != newSate.getBlock());
 	}
 }
