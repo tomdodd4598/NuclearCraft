572d0c6c4d271cd5e1cfd9a0716e145e052d027a
Shadows_of_Fire <Bward7864@gmail.com>
2019-04-26 02:39:08 -0400

    Remove extra info from ItemInfo

--- src/main/java/nc/util/ItemInfo.java
+++ src/main/java/nc/util/ItemInfo.java
@@ -1,42 +1,40 @@
 package nc.util;
 
-import java.util.Objects;
-
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class ItemInfo {
 	
-	private final boolean isEmpty;
 	private final Item item;
 	private final int meta;
+	private final int hash;
 	
 	public ItemInfo(ItemStack stack) {
-		isEmpty = stack.isEmpty();
 		item = stack.getItem();
-		meta = stack.getItemDamage();
+		meta = item.getHasSubtypes() ? 0 : stack.getMetadata();
+		hash = Item.REGISTRY.getIDForObject(item) << 16 | meta & 65535;
 	}
 	
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof ItemStack) {
 			ItemStack stack = (ItemStack)obj;
-			return !stack.isEmpty() && stack.getItem() == item && stack.getItemDamage() == meta;
+			return stack.getItem() == item && stack.getMetadata() == meta;
 		}
 		else if (obj instanceof ItemInfo) {
 			ItemInfo other = (ItemInfo)obj;
-			return !other.isEmpty && other.item == item && other.meta == meta;
+			return other.hash == hash;
 		}
 		return super.equals(obj);
 	}
 	
 	@Override
 	public String toString() {
-		return isEmpty ? "empty" : item.getRegistryName().toString() + ":" + meta;
+		return item.getRegistryName().toString() + ":" + meta;
 	}
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(isEmpty, item, meta);
+		return hash;
 	}
 }
