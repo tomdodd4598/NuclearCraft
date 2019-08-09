ff1791685344000ea9c2b7aae15f8525746e42bb
D AHS <34786330+ChlorineX@users.noreply.github.com>
2019-08-09 19:01:44 +0800

    Update Global.java

--- src/main/java/nc/Global.java
+++ src/main/java/nc/Global.java
@@ -6,7 +6,7 @@ public class Global {
 	public static final String MOD_SHORT_ID = "nc";
 	public static final String MOD_NAME = "NuclearCraft";
 	public static final String VERSION = "@VERSION@";
-	public static final String DEPENDENCIES = "after:tconstruct;after:conarm;after:openterraingenerator";
+	public static final String DEPENDENCIES = "after:tconstruct;after:conarm;after:openterraingenerator;after:alchemistry";
 
 	public static final String NC_CLIENT_PROXY = "nc.proxy.ClientProxy";
 	public static final String NC_COMMON_PROXY = "nc.proxy.CommonProxy";
