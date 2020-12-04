56bf8c17596d2c3ddf349061ae2802cb8a01972d
ITman <igentuman@gmail.com>
2020-12-04 12:17:12 +0300

    Custom triggers for multiblock forming event

--- src/main/java/nc/proxy/CommonProxy.java
+++ src/main/java/nc/proxy/CommonProxy.java
@@ -147,6 +147,7 @@ public class CommonProxy {
 		for (RegistrationInfo info : CTRegistration.INFO_LIST) {
 			info.init();
 		}
+		//NCAdvancements.init();
 	}
 	
 	public void postInit(FMLPostInitializationEvent postEvent) {
