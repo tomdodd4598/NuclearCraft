794bc15937a0bf7d07757a174ec882620491bf4b
Thiz <Thizthizzydizzy@gmail.com>
2025-03-07 19:08:46 -0500

    NCPF export - Removed modules from configuration container

--- src/main/java/net/ncplanner/ncpf/NCPFConfigurationContainer.java
+++ src/main/java/net/ncplanner/ncpf/NCPFConfigurationContainer.java
@@ -1,8 +1,4 @@
 package net.ncplanner.ncpf;
 import java.util.HashMap;
 public class NCPFConfigurationContainer extends HashMap<String, Object>{
-    public NCPFModuleList modules = new NCPFModuleList();
-    {
-        put("modules", modules);
-    }
 }
