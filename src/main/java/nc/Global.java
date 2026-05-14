5c75ddc22090dad2fa1948f57ed32cad85b86858
Tom Dodd <joedodd35@gmail.com>
2026-05-14 14:02:52 +0100

    Radiation system fixes

--- src/main/java/nc/Global.java
+++ src/main/java/nc/Global.java
@@ -5,7 +5,7 @@ public class Global {
 	public static final String MOD_ID = "nuclearcraft";
 	public static final String MOD_SHORT_ID = "nc";
 	public static final String MOD_NAME = "NuclearCraft";
-	public static final String VERSION = "2.19a";
+	public static final String VERSION = "2.19b";
 	public static final String DEPENDENCIES = "required-after:forge;" + "after:tconstruct;after:conarm;after:openterraingenerator;after:alchemistry";
 	
 	public static final String NC_CLIENT_PROXY = "nc.proxy.ClientProxy";
--- src/main/java/nc/radiation/RadiationHelper.java
+++ src/main/java/nc/radiation/RadiationHelper.java
@@ -30,6 +30,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.storage.loot.ILootContainer;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
@@ -155,7 +156,7 @@ public class RadiationHelper {
 		
 		if (NCConfig.radiation_hardcore_containers > 0D) {
 			IItemHandler inventory = getTileInventory(provider, side);
-			if (inventory != null) {
+			if (inventory != null && !(provider instanceof ILootContainer && ((ILootContainer) provider).getLootTable() != null)) {
 				for (int i = 0; i < inventory.getSlots(); i++) {
 					ItemStack stack = inventory.getStackInSlot(i);
 					rawRadiation += getRadiationFromStack(stack, NCConfig.radiation_hardcore_containers);
